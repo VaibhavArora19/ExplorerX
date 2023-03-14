0898e64669e089c5216c1eeff399266a8510eea9
Aman-Mandal <002amankumar@gmail.com>
2023-03-14 18:25:44 +0530

    Added basic contract details page

---
+++ frontend/components/Deploy/SelectChain.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SelectChain = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default SelectChain
---
+++ frontend/pages/deploy/index.js
@@ -0,0 +1,14 @@
+import ContractDetails from '@/components/Deploy/ContractDetails'
+import Introduction from '@/components/Deploy/Introduction'
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='bg-[#171717] h-screen flex items-center justify-center'>
+        {/* <Introduction/> */}
+        {/* <ContractDetails/> */}
+    </div>
+  )
+}
+
+export default index

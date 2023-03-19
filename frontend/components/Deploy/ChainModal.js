7db57db5ec24d1ee293f883d06cf316c3dadd5c9
Dinesh <dineshaitham2@gmail.com>
2023-03-19 19:30:16 +0530

    added args

--- frontend/components/Deploy/ChainModal.js
+++ frontend/components/Deploy/ChainModal.js
@@ -1,104 +1,114 @@
-import Image from 'next/image'
-import React, { useState } from 'react'
-import polygonSvg from '../../public/assets/deploy/polygon.svg'
-import gnosisImg from '../../public/assets/deploy/gnosisImg.png'
-import scrollImg from '../../public/assets/deploy/scrollImg.png'
-import zksyncImg from '../../public/assets/deploy/ZKsyncImg.png'
-import optimismImg from '../../public/assets/deploy/optimism.png'
-import fvmImg from '../../public/assets/deploy/fvm.png'
+import Image from "next/image";
+import React, { useState } from "react";
+import polygonSvg from "../../public/assets/deploy/polygon.svg";
+import gnosisImg from "../../public/assets/deploy/gnosisImg.png";
+import scrollImg from "../../public/assets/deploy/scrollImg.png";
+import zksyncImg from "../../public/assets/deploy/ZKsyncImg.png";
+import optimismImg from "../../public/assets/deploy/optimism.png";
+import fvmImg from "../../public/assets/deploy/fvm.png";
 
-const Backdrop = ({onClose}) => {
-    return <div onClick={onClose} className='top-0 left-0 fixed bg-black/20 backdrop-blur-md h-screen w-screen'></div>
-}
+const Backdrop = ({ onClose }) => {
+  return (
+    <div
+      onClick={onClose}
+      className="top-0 left-0 fixed bg-black/20 backdrop-blur-md h-screen w-screen"
+    ></div>
+  );
+};
 
 const chains = [
-    {
-        id: 'c1',
-        chainImg: polygonSvg,
-        chainName: 'Polygon',
-        chainAdd: '0xq090190310zja8q9409qe2420' 
-    },
-    {
-        id: 'c2',
-        chainImg: gnosisImg,
-        chainName: 'Gnosis',
-        chainAdd: '0xq090190310zja8q9409qe2420' 
-    },
-    {
-        id: 'c3',
-        chainImg: scrollImg,
-        chainName: 'Scroll',
-        chainAdd: '0xq090190310zja8q9409qe2420' 
-    },
-    {
-        id: 'c4',
-        chainImg: optimismImg,
-        chainName: 'Optimism',
-        chainAdd: '0xq090190310zja8q9409qe2420' 
-    },{
-        id: 'c5',
-        chainImg: fvmImg,
-        chainName: 'FVM',
-        chainAdd: '0xq090190310zja8q9409qe2420' 
-    },
-    {
-        id: 'c6',
-        chainImg: zksyncImg,
-        chainName: 'ZKSync',
-        chainAdd: '0xq090190310zja8q9409qe2420' 
-    },
-    {
-        id: 'c7',
-        chainImg: polygonSvg,
-        chainName: 'Mantle',
-        chainAdd: '0xq090190310zja8q9409qe2420' 
-    },
-    {
-        id: 'c8',
-        chainImg: polygonSvg,
-        chainName: 'Sepolia',
-        chainAdd: '0xq090190310zja8q9409qe2420' 
-    }
+  {
+    id: "c1",
+    chainImg: polygonSvg,
+    chainName: "Polygon",
+    chainAdd: "0xq090190310zja8q9409qe2420",
+  },
+  {
+    id: "c2",
+    chainImg: gnosisImg,
+    chainName: "Gnosis",
+    chainAdd: "0xq090190310zja8q9409qe2420",
+  },
+  {
+    id: "c3",
+    chainImg: scrollImg,
+    chainName: "Scroll",
+    chainAdd: "0xq090190310zja8q9409qe2420",
+  },
+  {
+    id: "c4",
+    chainImg: optimismImg,
+    chainName: "Optimism",
+    chainAdd: "0xq090190310zja8q9409qe2420",
+  },
+  {
+    id: "c5",
+    chainImg: fvmImg,
+    chainName: "FVM",
+    chainAdd: "0xq090190310zja8q9409qe2420",
+  },
+  {
+    id: "c6",
+    chainImg: zksyncImg,
+    chainName: "ZKSync",
+    chainAdd: "0xq090190310zja8q9409qe2420",
+  },
+  {
+    id: "c7",
+    chainImg: polygonSvg,
+    chainName: "Mantle",
+    chainAdd: "0xq090190310zja8q9409qe2420",
+  },
+  {
+    id: "c8",
+    chainImg: polygonSvg,
+    chainName: "Sepolia",
+    chainAdd: "0xq090190310zja8q9409qe2420",
+  },
+];
 
-]
+const ChainModal = ({ onClose, sendData }) => {
+  // const [chain, setChain] = useState(null)
 
-const ChainModal = ({onClose, sendData}) => {
-
-    // const [chain, setChain] = useState(null)
-
-    const onClick = (chain) => {
-        const data = {
-            chainName: chain.chainName,
-            chainImg: chain.chainImg,
-            chainAdd: chain.chainAdd
-        }
-        sendData(data)
-        onClose()
-    }
+  const onClick = (chain) => {
+    const data = {
+      chainName: chain.chainName,
+      chainImg: chain.chainImg,
+      chainAdd: chain.chainAdd,
+    };
+    sendData(data);
+    onClose();
+  };
 
   return (
     <>
-    <Backdrop onClose={onClose} />
-    <div className='w-[450px] rounded-2xl absolute top-[50%] left-[46%] shadow-md -translate-x-[50%] -translate-y-[50%] z-10 rounded-b-2xl  overflow-hidden border border-gray-800'>
-        <div className='bg-[#1D40AF] pt-6 pb-10 px-4 w-full rounded-t-2xl'>
-            <h2 className='text-white font-semibold'>Select a Chain</h2>
+      <Backdrop onClose={onClose} />
+      <div className="w-[450px] rounded-2xl absolute top-[50%] left-[46%] shadow-md -translate-x-[50%] -translate-y-[50%] z-10 rounded-b-2xl  overflow-hidden border border-gray-800">
+        <div className="bg-[#1D40AF] pt-6 pb-10 px-4 w-full rounded-t-2xl">
+          <h2 className="text-white font-semibold">Select a Chain</h2>
         </div>
-        <div className=' bg-[#151515] p-3 text-white flex flex-col gap-2 overflow-y-scroll max-h-[350px]'>
-        {chains.map((chain, i) => (
-            <div onClick={() => onClick(chain)}
-            className='py-3 px-3 flex gap-4 hover:bg-[#323131] cursor-pointer rounded-xl'>
-                <Image src={chain.chainImg} alt={chain.chainName} width={40} height={40} />
-                <div>
-                    <h3 className='font-semibold'>{chain.chainName}</h3>
-                    <p className='text-[12px] tracking-wide text-gray-500'>{chain.chainAdd}</p>
-                </div>
+        <div className=" bg-[#151515] p-3 text-white flex flex-col gap-2 overflow-y-scroll max-h-[350px]">
+          {chains.map((chain, i) => (
+            <div
+              onClick={() => onClick(chain)}
+              className="py-3 px-3 flex gap-4 hover:bg-[#323131] cursor-pointer rounded-xl"
+            >
+              <Image
+                src={chain.chainImg}
+                alt={chain.chainName}
+                width={40}
+                height={40}
+              />
+              <div className="flex items-center">
+                <h3 className="font-semibold">{chain.chainName}</h3>
+                {/* <p className='text-[12px] tracking-wide text-gray-500'>{chain.chainAdd}</p> */}
+              </div>
             </div>
-        ))}
-            
+          ))}
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ChainModal
+export default ChainModal;
--- frontend/components/Deploy/SelectChain.js
+++ frontend/components/Deploy/SelectChain.js
@@ -1,114 +1,185 @@
-import Image from 'next/image'
-import React, { use, useState } from 'react'
-import {GiBreakingChain} from 'react-icons/gi'
-import { MdKeyboardArrowUp, MdKeyboardArrowDown} from 'react-icons/md'
-import polygonSvg from '../../public/assets/deploy/polygon.svg'
-import ChainModal from './ChainModal'
+import Image from "next/image";
+import React, { use, useState } from "react";
+import { GiBreakingChain } from "react-icons/gi";
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
+import polygonSvg from "../../public/assets/deploy/polygon.svg";
+import ChainModal from "./ChainModal";
+import { useWeb3Modal } from "@web3modal/react";
+import { useAccount } from "wagmi";
 
-const SelectChain = ({setPage, page, formData, setFormData}) => {
-    const [isMultichain, setIsMultichain] = useState(false)
-    const [isSinlgeChain, setIsSingleChain] = useState(false)
-    const [openModal, setOpenModal] = useState(false)
-    const [chain, setChain] = useState({
-        chainName: '',
-        chainImg: '',
-        chainAdd: ''
-    })
+const SelectChain = ({ setPage, page, formData, setFormData }) => {
+  const [isMultichain, setIsMultichain] = useState(false);
+  const [isSinlgeChain, setIsSingleChain] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [chain, setChain] = useState({
+    chainName: "",
+    chainImg: "",
+    chainAdd: "",
+  });
 
-    const sendChain = (chain) => {
-        setChain(chain)
-        console.log(chain)
+  const { isOpen, open, close, setDefaultChain } = useWeb3Modal();
+  const { account, isConnected } = useAccount();
+
+  const connectWalletHandler = async () => {
+    try {
+      setDefaultChain(chain.chainName);
+      await open();
+    } catch (err) {
+      console.log(err, "wallet  connected");
+    }
+  };
+
+  const sendChain = (chain) => {
+    setChain(chain);
+    console.log(chain);
+  };
+
+  const multichainHandler = () => {
+    setIsSingleChain(false);
+    setIsMultichain(true);
+  };
+
+  const singleChainHandler = () => {
+    setIsMultichain(false);
+    setIsSingleChain(true);
+  };
+
+  const nextPageHandler = () => {
+    if (chain.chainName.length) {
+      setFormData({ ...formData, currentDeployChain: chain });
     }
 
-    const multichainHandler = () => {
-        setIsSingleChain(false)
-        setIsMultichain(true)
+    if (isSinlgeChain) {
+      setPage((currPage) => currPage + 2);
+    } else if (isMultichain) {
+      setPage((currPage) => currPage + 1);
+    } else {
+      alert("Select single or multiple");
     }
+  };
 
-    const singleChainHandler =() => {
-        setIsMultichain(false)
-        setIsSingleChain(true) 
-    }
+  const previousPageHandler = () => {
+    setPage((currPage) => currPage - 1);
+  };
 
-    const nextPageHandler = () => {
+  const openModalHandler = () => {
+    setOpenModal(true);
+  };
 
-        if(chain.chainName.length){
-            setFormData({...formData, currentDeployChain: chain})
-        }
-
-        if(isSinlgeChain){
-            setPage((currPage) => currPage + 2)
-        }else if (isMultichain){
-            setPage((currPage) => currPage + 1)
-        }
-        else{
-        alert('Select single or multiple')
-        }
-      }
-    
-      const previousPageHandler = () => {
-        setPage((currPage) => currPage - 1)
-      }
-
-      const openModalHandler = () => {
-        setOpenModal(true)
-      }
-
-      const closeModalHandler = () => {
-        setOpenModal(false)
-      }
+  const closeModalHandler = () => {
+    setOpenModal(false);
+  };
 
   return (
-    <div className='text-white w-[550px] bg-[#1E1E1E] py-10 px-10 rounded-2xl border border-gray-700'>
-        <h2 className='text-2xl font-semibold mb-7'>Select Chains</h2>
+    <div className="text-white w-[550px] bg-[#1E1E1E] py-10 px-10 rounded-2xl border border-gray-700">
+      <h2 className="text-2xl font-semibold mb-7">Select Chains</h2>
 
-        <form className='flex flex-col '>
-            <p className='text-sm text-gray-400 mb-1'>Current chain</p>
+      <form className="flex flex-col ">
+        <p className="text-sm text-gray-400 mb-1">Deploy From</p>
 
-            <div onClick={openModalHandler} className='py-3 px-3 border border-gray-700 rounded-md flex justify-between mb-6 hover:bg-[#272626] cursor-pointer'>
-                
-                <div className='flex gap-3'>
-                <Image src={!chain.length? chain.chainImg : polygonSvg} alt={!chain.length ? chain.chainName: 'Chain'} width={40} height={40} />
-                <div>
-                    <h3 className='font-semibold'>{!chain.length? chain.chainName : 'Select chain'}</h3>
-                    <p className='text-[12px] tracking-wide text-gray-500'>{!chain.length ? chain.chainAdd : '0x00000000000000000000000'}</p>
-                </div>
-                </div>
-                
-                <div className='ml-7 flex flex-col items-center justify-center bg-[#2E2E2E] px-2 rounded-md'>
-                    <MdKeyboardArrowUp size={20}/>
-                    <MdKeyboardArrowDown size={20}/>
-                </div>
+        <div
+          onClick={openModalHandler}
+          className="py-3 px-3 border border-gray-700 rounded-md flex justify-between mb-6 hover:bg-[#272626] cursor-pointer"
+        >
+          <div className="flex gap-2">
+            {chain.chainImg != "" && (
+              <Image
+                src={!chain.length ? chain.chainImg : polygonSvg}
+                alt={!chain.length ? chain.chainName : "Chain"}
+                width={40}
+                height={40}
+              />
+            )}
+            <div className="flex items-center">
+              <h3 className="font-semibold ml-2">
+                {chain.chainName != "" ? chain.chainName : "Select chain"}
+              </h3>
+              {/* <p className="text-[12px] tracking-wide text-gray-500">
+                {!chain.length ? chain.chainAdd : "0x00000000000000000000000"}
+              </p> */}
             </div>
+          </div>
 
-            <p className='text-sm text-gray-400 mb-2'>Deploy type</p>
+          <div className="ml-7 flex flex-col items-center justify-center bg-[#2E2E2E] px-2 rounded-md">
+            <MdKeyboardArrowUp size={20} />
+            <MdKeyboardArrowDown size={20} />
+          </div>
+        </div>
 
-            <div className='flex gap-4'>
-                <div onClick={singleChainHandler} className={` ${isSinlgeChain ? 'bg-[#22335F]': 'bg-[#363636]'}  p-10 rounded-xl flex-[0.5] cursor-pointer`}>
-                    <div className='bg-[#171717] rounded-md p-4 w-fit mb-7'>
-                        <GiBreakingChain className={`${isSinlgeChain && 'text-blue-300'}`} size={20}/>
-                    </div>
-                    <p className={`${isSinlgeChain && 'text-blue-300 '} font-medium`}>Single Chain</p>
-                </div>
+        <p className="text-sm text-gray-400 mb-2">Deploy type</p>
 
-                <div onClick={multichainHandler} className={` ${isMultichain ? 'bg-[#1F423A]': 'bg-[#363636]'} p-10 rounded-xl flex-[0.5] cursor-pointer`}>
-                    <div className='bg-[#171717] rounded-md p-4 w-fit mb-7'>
-                        <GiBreakingChain size={20} className={`${isMultichain ? 'text-green-400': ''}`}/>
-                    </div>
-                    <p className={`${isMultichain ? 'text-green-400': ''} font-medium`}>Multi Chain</p>
-                </div>
+        <div className="flex gap-4">
+          <div
+            onClick={singleChainHandler}
+            className={` ${
+              isSinlgeChain ? "bg-[#22335F]" : "bg-[#363636]"
+            }  p-10 rounded-xl flex-[0.5] cursor-pointer`}
+          >
+            <div className="bg-[#171717] rounded-md p-4 w-fit mb-7">
+              <GiBreakingChain
+                className={`${isSinlgeChain && "text-blue-300"}`}
+                size={20}
+              />
             </div>
+            <p className={`${isSinlgeChain && "text-blue-300 "} font-medium`}>
+              Single Chain
+            </p>
+          </div>
 
-
-            <div className='flex justify-between mt-6'>
-                <button onClick={previousPageHandler} type='button' className='py-3 px-7 rounded-md bg-[#292929] text-gray-300 border border-gray-600'>Back</button>
-                <button onClick={nextPageHandler} type='button' className='py-3 px-7 rounded-md bg-[#292929] text-gray-300 border border-gray-600'>Next</button>
+          <div
+            onClick={multichainHandler}
+            className={` ${
+              isMultichain ? "bg-[#1F423A]" : "bg-[#363636]"
+            } p-10 rounded-xl flex-[0.5] cursor-pointer`}
+          >
+            <div className="bg-[#171717] rounded-md p-4 w-fit mb-7">
+              <GiBreakingChain
+                size={20}
+                className={`${isMultichain ? "text-green-400" : ""}`}
+              />
             </div>
-        </form>
-        
-        {openModal && <ChainModal sendData={sendChain} onClose={closeModalHandler}/>}
+            <p
+              className={`${isMultichain ? "text-green-400" : ""} font-medium`}
+            >
+              Multi Chain
+            </p>
+          </div>
+        </div>
+
+        <div className="flex justify-between mt-6">
+          <button
+            onClick={previousPageHandler}
+            type="button"
+            className="py-3 px-7 rounded-md bg-[#292929] text-gray-300 border border-gray-600"
+          >
+            Back
+          </button>
+
+          {!isConnected ? (
+            <button
+              onClick={connectWalletHandler}
+              type="button"
+              className="py-3 px-7 rounded-md bg-[#292929] text-gray-300 border border-gray-600"
+            >
+              Connect Wallet
+            </button>
+          ) : (
+            <button
+              onClick={nextPageHandler}
+              type="button"
+              className="py-3 px-7 rounded-md bg-[#292929] text-gray-300 border border-gray-600"
+            >
+              Next
+            </button>
+          )}
+        </div>
+      </form>
+
+      {openModal && (
+        <ChainModal sendData={sendChain} onClose={closeModalHandler} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default SelectChain
+export default SelectChain;

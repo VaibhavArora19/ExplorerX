d3741b42aa7c842f167a35aadd88f863f42d62bf
Aman-Mandal <002amankumar@gmail.com>
2023-03-20 23:37:12 +0530

    Added landing page hero section

---
+++ frontend/components/Home/Navbar.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <Link href='/'></Link>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar

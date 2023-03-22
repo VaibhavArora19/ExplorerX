aa7a6ebf793cadfce34501fab9318c6c8001c26e
Aman-Mandal <002amankumar@gmail.com>
2023-03-22 20:52:01 +0530

    Added styling to explorer page

--- frontend/components/Home/Hero.js
+++ frontend/components/Home/Hero.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
   return (
     <section className="flex flex-col gap-1 h-[94vh] px-32 justify-center items-start font-Poppins">
-      <h2 className="text-white text-9xl tracking-wide font-medium">
+      <h2 className="text-white  text-9xl tracking-wide font-medium">
         ExplorerX.
       </h2>
       <p className="text-8xl pt-3  bg-gradient-to-r from-[#343434] font-medium to-[#6a6a6a] bg-clip-text text-transparent ">
